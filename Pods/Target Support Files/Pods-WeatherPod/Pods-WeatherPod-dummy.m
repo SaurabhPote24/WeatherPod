ec97b95d1851403c893b011e774c58d3304eae9d
saurabh pote <saurabh@saurabhs-MacBook-Pro.local>
2021-12-09 12:24:59 +0530

    Initial commit

---
+++ Pods/Target Support Files/Pods-WeatherPod/Pods-WeatherPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeatherPod : NSObject
+@end
+@implementation PodsDummy_Pods_WeatherPod
+@end
---
+++ WeatherPod/WeatherPod.h
@@ -0,0 +1,18 @@
+//
+//  WeatherPod.h
+//  WeatherPod
+//
+//  Created by saurabh pote on 09/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherPod.
+FOUNDATION_EXPORT double WeatherPodVersionNumber;
+
+//! Project version string for WeatherPod.
+FOUNDATION_EXPORT const unsigned char WeatherPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherPod/PublicHeader.h>
+
+
